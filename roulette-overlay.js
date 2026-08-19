7278a7ccd126eb59c3ce4748ac18ccae3b26f836
KwiecienMarcin <47104423+KwiecienMarcin@users.noreply.github.com>
2026-08-19 23:36:49 +0200

    Update roulette-overlay.js

--- roulette-overlay.js
+++ roulette-overlay.js
@@ -3,7 +3,7 @@
 // Zmien RLT_API na docelowy adres backendu (Railway) przed wdrozeniem produkcyjnym.
 (function () {
   const RLT_API = window.RLT_API_OVERRIDE || 'http://192.168.0.245:4001';
-  const RLT_POLL_MS = 8000;
+  const RLT_POLL_MS = 3000;
   const RLT_DEV_BOARD = new URLSearchParams(location.search).get('devBoard');
   const SKIP_DELAY_MS = 5000;
   const CIRCLE_ORDER = [20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5];
